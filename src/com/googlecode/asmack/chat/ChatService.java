38a9d9da849684abaaf1ca68bc0d9fcee6dde56b
Rene Treffer <treffer@measite.de>
2011-01-04 18:04:04 +0000

    Add missing javadoc

--- src/com/googlecode/asmack/chat/ChatService.java
+++ src/com/googlecode/asmack/chat/ChatService.java
@@ -12,23 +12,43 @@ import com.googlecode.asmack.connection.IXmppTransportService;
  */
 public class ChatService extends AsmackClientService {
 
+    /**
+     * Create a new chat service.
+     */
     public ChatService() {
         super(R.xml.smackproviders);
     }
 
+    /**
+     * Called whenever the transport service is reachable.
+     * @param service The remote xmpp transport service.
+     */
     @Override
     public void onTrasportServiceConnect(IXmppTransportService service) {
     }
 
+    /**
+     * Called whenever the transport service connection dies.
+     * @param service The remote xmpp transport service.
+     */
     @Override
     public void onTrasportServiceDisconnect(IXmppTransportService service) {
     }
 
+    /**
+     * Called whenever another context based android class tries to bind to this
+     * service. Currently return null.
+     * @param intent The incoming intent, ignored.
+     */
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
 
+    /**
+     * Called whenever a client becomes ready but is not yet registered. This
+     * method wires up listeners to the client.
+     */
     @Override
     protected void preClientStart() {
         client.registerListener(
@@ -36,6 +56,11 @@ public class ChatService extends AsmackClientService {
         );
     }
 
+    /**
+     * Called when this service starts.
+     * @param intent The incoming intent.
+     * @param startId The start id.
+     */
     @Override
     public void onStart(Intent intent, int startId) {
         super.onStart(intent, startId);
